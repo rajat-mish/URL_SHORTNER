5b9818070aceb35380a36c6a2d58e39b5ea1ab75
rajat <rajatmishra6767@gmail.com>
2025-06-14 00:47:54 +0530

    Add logging

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -1,20 +1,23 @@
 import { verifyToken } from "../utils/helper.js";
 import { findUserById } from "../dao/user.dao.js";
 
-export const authMiddleware = async (req, res, next) => {
+export const  authMiddleware = async (req, res, next) => {
   const token = req.cookies.accessToken;
   if (!token) {
+    console.log("Invalid token"+token);
     return res.status(401).json({ message: "Unauthorized" });
   }
   try {
     const decoded = verifyToken(token);
     const user = await findUserById(decoded.userId);
     if (!user) {
+      console.log("User not found");
       return res.status(401).json({ message: "Unauthorized" });
     }
     req.user = user;
     next();
   } catch (error) {
+    console.log('Something went wrong');
     return res.status(401).json({ message: "Unauthorized" });
   }
 };
