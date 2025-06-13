0d3ecde925cfcecaac5b4d1244e2bbac21c3c6e2
rajat <rajatmishra6767@gmail.com>
2025-06-14 00:58:39 +0530

    Update config

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -1,6 +1,6 @@
 export const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'Lax',
+  sameSite: 'None',
   maxAge: 1000 * 60 * 60, // 5 minutes
 };
